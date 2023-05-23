bd0b98b90a24aa63ebfa674a4477cf098cc719ee
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-23 16:36:46 +0300

    create tours component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = 'https://course-api.com/react-tours-project';
 
@@ -11,17 +13,25 @@ const App = () => {
     try {
       const response = await fetch(url);
       const tours = await response.json();
-      console.log(tours);
+      setTours(tours);
     } catch (error) {
       console.log(error)
     }
-    
+    setIsLoading(false)
   }
 
   useEffect(() => {
     fetchTours()
   }, [])
 
-  return <h2>Tours Starter</h2>;
+  if (isLoading) {
+    return <main>
+        <Loading />
+    </main>
+  }
+
+  return <main>
+    <Tours tours={tours}/>
+  </main>;
 };
 export default App;
