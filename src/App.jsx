4a91f51f1f97136a72d12d825dc22038b07d0025
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-23 16:15:15 +0300

    configure states and effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
