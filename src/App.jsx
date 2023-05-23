f35c6186acd629e90b646a71c23b8db52de2133a
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-23 16:52:46 +0300

    add remove button to tour components

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setIsLoading(true)
     try {
@@ -31,7 +36,7 @@ const App = () => {
   }
 
   return <main>
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour}/>
   </main>;
 };
 export default App;
